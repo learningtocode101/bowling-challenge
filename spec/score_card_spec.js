ce67e5bcc4997b50ac68d2d96d1c5283e95b041d
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-20 14:53:56 +0000

    updated roll method

--- spec/score_card_spec.js
+++ spec/score_card_spec.js
@@ -1,13 +1,12 @@
 describe('Bowling Scorecard', ()=>{
   describe('Gutter game', ()=>{
     scorecard1 = new ScoreCard
-    it('returns zero if user gutters ball for all 20 frame', ()=>{
+    it('returns zero if user gutters ball for all 10 frames', ()=>{
       for(let i =0; i <=20; i++){
-        scorecard1.ball_roll(20);
+        scorecard1.roll(0);
       }
       expect(scorecard1.is_complete).toBe(true);
       expect(scorecard1.final_score).toEqual(0);
     });
   });
-
 });
--- src/score_card.js
+++ src/score_card.js
@@ -4,12 +4,16 @@ class ScoreCard {
     this.roll_count = 0;
     this.current_frame = [];
     this.final_score = 0
+    this.frame_acc = 0;
   }
-  ball_roll(roll) {
+  roll(pins) {
     this.roll_count ++;
-    if (this.roll_count == 20) {
+    this.frame_acc ++;
+    this.current_frame.push(pins);
+    this.final_score += this.current_frame[0];
+    if (this.frame_acc == 20 && this.final_score == 0) {
       this.is_complete = true;
-      this.final_score = 0;
+      this.final_score;
     }
   }
 
